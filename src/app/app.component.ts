319d56b77a0501c2efd41def830cdc207c1953f4
AAvilan <andres.avilan@nurun.com>
2020-09-22 16:24:39 -0400

    Sending a POST Request - We add in the app.module.ts the HttpClientModule from '@angular/common/http' and inserted into the ngModule imports array. In the app.component.ts file, we inported the HTTPClient in the constructor and we modify the existing partially solution of the Http post in order to target the setup firebased. Important, some of the actions were already done and added in the first commit, so that is why the description.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+//Imported from the angular common http.
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,17 +10,25 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    //Injected to handle HTTP communications.
+    private http: HttpClient
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
+    //Testing if the postData was received.
+    //console.log(postData);
+
     this.http
       .post(
-        'https://ng-complete-guide-c56d3.firebaseio.com/posts.json',
+        'https://httpangular-a664a.firebaseio.com/posts.json',
         postData
       )
+      // In order to send the POST, Angular demands that we need to subscribe.
+      // It could be an empty subscription without the call back if we wanted.
       .subscribe(responseData => {
         console.log(responseData);
       });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+//Imported from the angular common http.
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    //The HttpClientModule was added to allow Angular handling of HTTP communications.
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
