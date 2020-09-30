90e9d12476efc352a157d6016f172e1ef7a53941
AAvilan <andres.avilan@nurun.com>
2020-09-30 10:08:32 -0400

    Response Interceptors - By using a method after the HttpHandler's handle method like the pipe, we can access or modify the HttpResponse inside of an Observable to do some logic. Be careful when the action to perform is to modify to avoid a break of the app. In this case, we access the response for console logging.

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,9 +1,11 @@
 import {
   HttpInterceptor,
   HttpRequest,
-  HttpHandler
+  HttpHandler,
+  HttpEventType
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 
 //We are modifying the provider's array on the app.module.ts, we don't need the injectable decorator.
 //I added the Injectable decorator to avoid breaking and to support non-previewable scenarios in the future.
@@ -26,6 +28,16 @@ export class AuthInterceptorService implements HttpInterceptor {
 
     //The HttpHandler with his method handle will allow the HttpRequest to continue it's flow to exit the application, make the Http call.
     //It is very important to have this in order to avoid any interruption on the calls.
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest)
+      .pipe(
+        //Tap gives us access to the HttpResponse without modifying it.
+        tap(
+        event => {
+          console.log(this.constructor.name + ' - HttpResponse event.', event);
+          if (event.type === HttpEventType.Response) {
+            console.log(this.constructor.name + ' - HttpResponse event Response body.', event.body);
+          }
+        }
+      ));
   }
 }
